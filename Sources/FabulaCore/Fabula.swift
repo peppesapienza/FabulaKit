da5fd6cbbe4314cdf96155dfbb849769378101fb
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2022-01-30 20:34:25 +1100

    Added `Event` as associatedtype of a `Fabula`

--- Sources/FabulaCore/Fabula.swift
+++ Sources/FabulaCore/Fabula.swift
@@ -4,26 +4,32 @@ import Foundation
 ///
 public protocol Fabula {
     associatedtype Body: Fabula
+    associatedtype Event: FabulaEvent
     
     @FabulaBuilder
     var body: Body { get }
-    
+        
     func run(in context: inout BotContext) throws
 }
 
-public extension Fabula {
-    func run(in context: inout BotContext) throws {
+extension Fabula {
+    public func run(in context: inout BotContext) throws {
         /// The base implementation does nothing.
     }
 }
+
+
 
 public protocol Container {
     var children: [AnyFabula] { get }
 }
 
 /// An helper extension to avoid accessing body of first level components
-extension Never: Fabula {
+extension Never: Fabula, FabulaEvent {
+    public typealias Event = Never
+
     public var body: Never { fatalError("no body in Never") }
+    public var id: UUID { UUID() }
 }
 
 extension Fabula where Body == Never {
