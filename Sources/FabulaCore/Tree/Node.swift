9e3bfa9117f87eeab3352b1b1998568da48511f0
Giuseppe Sapienza <sapienza.giuseppe93@gmail.com>
2022-02-09 08:04:05 +1100

    The node `content` and `add(child:)` are now internals

--- Sources/FabulaCore/Tree/Node.swift
+++ Sources/FabulaCore/Tree/Node.swift
@@ -1,4 +1,4 @@
-public final class Node {
+public class Node {
     
     init<F>(_ content: F, parent: Node? = nil) where F: Fabula {
         self.content = AnyFabula(content)
@@ -11,18 +11,13 @@ public final class Node {
     public var isRoot: Bool { parent == nil }
     public var isLeaf: Bool { children.isEmpty }
     
-    public let content: AnyFabula
+    let content: AnyFabula
     
     public var contentType: Any.Type {
         content.fabulaType
     }
     
-    public func add(child: Node) {
+    func add(child: Node) {
         children.append(child)
-    }
-    
-    /// Returns an iterator over the children of the current node
-    public func makeIterator() -> FabulaIterator {
-        FabulaIterator(self)
     }
 }
