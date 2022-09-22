926934504f0b9564d272f81ce4fb5a514fcd10db
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2022-09-22 11:51:27 +1000

    Removed `attributes`

--- Sources/FabulaCore/Tree/Node.swift
+++ Sources/FabulaCore/Tree/Node.swift
@@ -3,7 +3,6 @@ public class Node {
     init<F>(_ content: F, parent: Node? = nil) where F: Fabula {
         self.content = content as? AnyFabula ?? AnyFabula(content)
         self.parent = parent
-        self.attributes = parent?.attributes ?? []
     }
     
     var isRoot: Bool { parent == nil }
@@ -11,8 +10,7 @@ public class Node {
     
     private(set) weak var parent: Node?
     private(set) var children: [Node] = []
-    private (set) var attributes: [AnyAttribute] = []
-
+    
     let content: AnyFabula
     
     var contentType: Any.Type {
@@ -22,16 +20,4 @@ public class Node {
     func add(child: Node) {
         children.append(child)
     }
-    
-    func add(attributes: [AnyAttribute]) {
-        attributes.forEach { new in
-            guard let index = self.attributes.firstIndex(where: { $0.name == new.name && new.shouldReplaceExisting }) else {
-                self.attributes.append(new)
-                return
-            }
-            
-            self.attributes[index] = new
-        }
-    }
-    
 }
