02993c8142a4565fd2d0f3f14ff1f36ed603f86b
peppe.sapienza <sapienza.giuseppe93@gmail.com>
2022-09-25 11:49:50 +1000

    Replaced `buildBlock` with `buildPartialBlock`

--- Sources/FabulaCore/ResultBuilder.swift
+++ Sources/FabulaCore/ResultBuilder.swift
@@ -12,57 +12,13 @@ public struct FabulaBuilder {
 }
 
 public extension FabulaBuilder {
-    static func buildBlock<C0, C1>(_ c0: C0, _ c1: C1) -> some Fabula where C0: Fabula, C1: Fabula {
-        TupleFabula(c0, c1)
-    }
     
-    static func buildBlock<C0, C1, C2>(
-        _ c0: C0,
-        _ c1: C1,
-        _ c2: C2
-    ) -> some Fabula where C0: Fabula, C1: Fabula, C2: Fabula {
-        TupleFabula(c0, c1, c2)
-    }
+    static func buildPartialBlock<Content>(first content: Content) -> Content where Content: Fabula {
+            content
+        }
+
+        static func buildPartialBlock<C0, C1>(accumulated: C0, next: C1) -> TupleFabula<(C0, C1)> where C0: Fabula, C1: Fabula {
+            TupleFabula(accumulated, next)
+        }
     
-    static func buildBlock<C0, C1, C2, C3>(
-        _ c0: C0,
-        _ c1: C1,
-        _ c2: C2,
-        _ c3: C3
-    ) -> some Fabula where C0: Fabula, C1: Fabula, C2: Fabula, C3: Fabula {
-        TupleFabula(c0, c1, c2, c3)
-    }
-    
-    static func buildBlock<C0, C1, C2, C3, C4>(
-        _ c0: C0,
-        _ c1: C1,
-        _ c2: C2,
-        _ c3: C3,
-        _ c4: C4
-    ) -> some Fabula where C0: Fabula, C1: Fabula, C2: Fabula, C3: Fabula, C4: Fabula {
-        TupleFabula(c0, c1, c2, c3, c4)
-    }
-    
-    static func buildBlock<C0, C1, C2, C3, C4, C5>(
-        _ c0: C0,
-        _ c1: C1,
-        _ c2: C2,
-        _ c3: C3,
-        _ c4: C4,
-        _ c5: C5
-    ) -> some Fabula where C0: Fabula, C1: Fabula, C2: Fabula, C3: Fabula, C4: Fabula, C5: Fabula {
-        TupleFabula(c0, c1, c2, c3, c4, c5)
-    }
-    
-    static func buildBlock<C0, C1, C2, C3, C4, C5, C6>(
-        _ c0: C0,
-        _ c1: C1,
-        _ c2: C2,
-        _ c3: C3,
-        _ c4: C4,
-        _ c5: C5,
-        _ c6: C6
-    ) -> some Fabula where C0: Fabula, C1: Fabula, C2: Fabula, C3: Fabula, C4: Fabula, C5: Fabula, C6: Fabula {
-        TupleFabula(c0, c1, c2, c3, c4, c5, c6)
-    }
 }
